f615e1c43a03a2256558f38749dc5b7c82eeecd4
sensiblepuffin <175900+sensiblepuffin@users.noreply.github.com>
2025-07-03 12:56:01 -0400

    Select video URL and back out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Button, IconButton, TextField } from '@mui/material';
+import { Close, PlayArrow } from '@mui/icons-material';
 
 function App() {
+  const [videoURL, setVideoURL] = useState('');
+  const [videoSelected, setVideoSelected] = useState(false);
+  console.info('video', videoURL);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {(videoSelected && videoURL) ?
+        (<div>
+          <video
+          id="my-video"
+          // controls
+          preload="auto"
+          width="640"
+          height="480"
+          poster="MY_VIDEO_POSTER.jpg"
+          data-setup="{}"
         >
-          Learn React
-        </a>
-      </header>
+          <source src={videoSelected ? videoURL : 'null'} type="video/mp4"></source>
+          <p class="vjs-no-js">
+            To view this video please enable JavaScript, and consider upgrading to a
+            web browser that
+            <a href="https://videojs.com/html5-video-support/" target="_blank"
+            >supports HTML5 video</a
+            >
+          </p>
+        </video>
+        <IconButton>
+          <Close onClick={() => setVideoSelected(false)} />
+        </IconButton>
+        </div>) :
+        (<>
+            <p>Select a video first!</p>
+            <TextField id="outlined-basic" onChange={(evt) => setVideoURL(evt.target.value)} />
+            <IconButton>
+              <PlayArrow onClick={() => setVideoSelected(true)} />
+            </IconButton>
+        </>)}
     </div>
   );
 }
